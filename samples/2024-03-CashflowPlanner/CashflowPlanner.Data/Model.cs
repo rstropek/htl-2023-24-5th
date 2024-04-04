e0a0241b914a0fa7df27ea5c7efdeda0af5a2082
Rainer Stropek <rainer@software-architects.at>
2024-04-04 08:26:29 +0200

    Add cashflow planner exercise

---
+++ samples/2024-03-CashflowPlanner/CashflowPlanner.Data/Model.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CashflowPlanner.Data;
+
+[Index("Name", IsUnique = true)]
+public class Category
+{
+    public int ID { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+    
+    public char InOut { get; set; }
+}
+
+[Index("Name", IsUnique = true)]
+public class Currency
+{
+    public int ID { get; set; }
+
+    [MaxLength(3)]
+    public string Name { get; set; } = string.Empty;
+}
+
+[Index("FromCurrency", "ToCurrency", IsUnique = true)]
+public class ExchangeRate
+{     
+    public int ID { get; set; }
+
+    public string FromCurrency { get; set; } = string.Empty;
+
+    public string ToCurrency { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18, 4)")]
+    public decimal Rate { get; set; }
+}
+
+public class  Cashflow
+{
+    public int ID { get; set; }
+
+    public short Year { get; set; }
+
+    public byte Month { get; set; }
+
+    public int CategoryID { get; set; }
+
+    public Category? Category { get; set; }
+
+    public string Currency { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Amount { get; set; }
+}
---
+++ samples/2024-03-CashflowPlanner/CashflowPlanner/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace CashflowPlanner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow(MainWindowViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+
+        Loaded += async (sender, args) =>
+        {
+            try
+            {
+                await viewModel.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        };
+    }
+}
