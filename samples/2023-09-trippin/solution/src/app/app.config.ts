bc866922c62c1110eff46cc7f5152eb81228829d
Rainer <rainer@software-architects.at>
2024-09-22 19:31:43 +0200

    Update to ng 18

--- samples/2023-09-trippin/solution/src/app/app.config.ts
+++ samples/2023-09-trippin/solution/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, InjectionToken, importProvidersFrom } from '@angular
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const TRIPPIN_BASE_URL = new InjectionToken<string>('TrippinBaseUrl');
 
@@ -10,6 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     { provide: TRIPPIN_BASE_URL, useValue: 'https://services.odata.org/TripPinRESTierService/(S(m5bfpztyapemay4raovtk1wi))' },
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
   ],
 };
